2667dbcef2441ddd8a6db652e1ad64360eb83f13
mtank <mtankersley101@gmail.com>
2014-07-28 19:48:53 -0400

    Master of contollers
    master controller

---
+++ src/controllers/MasterController.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.ServletContext;
+import javax.servlet.RequestDispatcher;
+import daos.*;
+import dtos.*;
+/**
+ *
+ * @author Michael
+ */
+public class MasterController extends HttpServlet {
+    /**
+     * get method
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+        
+    }
+    
+    /**
+     * post method
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+}
